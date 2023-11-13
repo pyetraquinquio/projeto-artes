46efc06cd8eb4a9245a876b0a29b60ca0ea2a302
Pyh Quinquio <pyetraquinquio2007@gmail.com>
2023-11-13 11:55:01 -0300

    login

---
+++ app/page.jsx
@@ -0,0 +1,51 @@
+"use client"
+
+import styles from "./page.module.css"
+import Link from "next/link";
+
+
+
+
+export default function Login() {
+
+
+    return (
+        
+
+        <div className={styles.fundo}>
+
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+
+            <div className={styles.div}>
+
+                <h1 className={styles.h1}>Olá, Victor!</h1>
+
+             <br></br>
+                <br></br>
+
+            <nav>
+                <ul>
+                    <li>
+                    <Link className={styles.button} href="/principal">Visitar Site</Link>
+                    </li>
+                </ul>
+            </nav>
+               
+               
+
+            </div>
+
+            </div>
+
+    )
+}
